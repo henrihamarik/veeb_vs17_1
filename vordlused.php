da6fc2d886f4b1d32ae916964002c241d11c9e4b
Anna Karutina <anna.karutina@khk.ee>
2017-12-11 12:41:17 +0200

    Defineeritud vajalikud muutujad

---
+++ vordlused.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 11.12.2017
+ * Time: 12:38
+ */
+// defineeri 8 muutujat, mille väärtusteks
+// on täisarvud, reaalarvud, sõned ja
+// boolean väärtused
+$var1 = 5;
+$var2 = 5.0;
+$var3 = '5';
+$var4 = 2;
+$var5 = 2.0;
+$var6 = '2';
+$var7 = true;
+$var8 = false;
