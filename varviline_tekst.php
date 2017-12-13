fae581301fde579565c072f2ee163e9d7015500d
Anna Karutina <anna.karutina@khk.ee>
2017-12-13 15:10:24 +0200

    Kirjeldatud teksti värvi muutmine hex-süsteemis

--- varviline_tekst.php
+++ varviline_tekst.php
@@ -8,8 +8,9 @@
 for ($rida = 1; $rida <= 5; $rida++){
     $varv = '#';
     for($kord = 1; $kord <= 6; $kord++){
-        $juhuarv = rand(0, 9);
-        $varv = $varv.$juhuarv;
+        $juhuarv = rand(0, 15);
+        $juhuhex = dechex($juhuarv);
+        $varv = $varv.$juhuhex;
 //        echo $varv.'<br />';
     }
     echo '<font color="'.$varv.'">Värviline tekst</font><br />';
