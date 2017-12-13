fd90422875ab32fbe04a30d860cd7c3e41e13f23
Anna Karutina <anna.karutina@khk.ee>
2017-12-13 14:31:04 +0200

    for tsükkel on täiendatud continue operaatoriga

--- tsyklid.php
+++ tsyklid.php
@@ -8,8 +8,13 @@
 for($kord = 1; $kord <= 10; $kord++){
     if($kord % 2 == 0){
         $varv = 'red';
+        continue;
     } else {
         $varv = 'blue';
     }
+//    kuna varvi defineerimisel juba on kirjas
+//    if($varv == 'red'){
+//        continue;
+//    }
     echo '<font color="'.$varv.'">'.$kord.'</font><br />';
 }
