5582b2ec15fca8f151ae45851a75d03141ffbc69
Anna Karutina <anna.karutina@khk.ee>
2017-12-15 10:28:13 +0200

    Loodud tekstiline tabel kasutades dünaamilise muutuja defineerimist

--- funktsioonid.php
+++ funktsioonid.php
@@ -21,6 +21,9 @@
 // täineda looTabel funktsioon nii, et tabeli pesa
 // taustavärv oleks juhuslikult genereeritud
 
+// lisada lehe automaatne taaskäivitamine
+header('Refresh: 0.2');
+
 function genereeriVarv(){
     $varv = '#';
     for($kord = 1; $kord <= 6; $kord++){
@@ -45,3 +48,22 @@ function looTabel($ridadeArv, $veergudeArv){
     echo '</table>';
 }
 looTabel(5, 7);
+
+// Ülesanne
+// Loo funktsioon nimega tabelTekst, mis võtab parameetrina
+// 4 sõna ja väljastab 4-realise tabeli, mille
+// pesades on parameetritega edastatud sõnad
+// NB! Tabeli loomisel tuleb hakkama saada tsüklidega ;)
+// ja muutujate dünaamilise defineerimisega
+function tabelTekst($sona1, $sona2, $sona3, $sona4){
+    echo '<table border="1">';
+    for($reaNumber = 1; $reaNumber <= 4; $reaNumber++){
+        echo '<tr>';
+            echo '<td>';
+                echo ${'sona'.$reaNumber};
+            echo '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+}
+tabelTekst('See', 'on', 'üks', 'tabel');
