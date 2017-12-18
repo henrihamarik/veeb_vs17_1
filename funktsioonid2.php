5abefab2de160c2408d48453aa0dfe9d4f859566
Anna Karutina <anna.karutina@khk.ee>
2017-12-18 12:35:35 +0200

    Kirjeldatud arvude leidmise algoritm

---
+++ funktsioonid2.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 18.12.2017
+ * Time: 12:02
+ */
+
+/*
+ * 1. Koostada funktsioon nimega arvuSumma, mis võtab
+ * parameetrina suvalise numbri ja arvutab numbri
+ * arvude summat - juhul, kui on argumendina
+ * funktsioonile antud number 123, siis programm
+ * peab leidma 1+2+3 summa, ehk 6.
+ * Lahendamiseks ei tohi kasutada sõnetöötluse võimalused.
+ * Funktsioon peab tagastama leitud summa põhiprogrammile.
+ * Põhiprogrammis kasutada väljatrükis tagastatud väärtus koos
+ * selgitava tekstiga
+ * */
+function arvuSumma($number){
+    $summa = 0;
+    while($number != 0){
+        $arv = $number % 10;
+        echo 'arv = '.$arv.'<br />';
+        $number = $number / 10;
+        echo 'number = '.$number.'<br />';
+        settype($number, 'integer');
+        echo 'number = '.$number.'<br />';
+    }
+}
+arvuSumma(1235);
