b1bbe63e87fe8669b5c8e3bfe72a85719f3c5fda
Anna Karutina <anna.karutina@khk.ee>
2017-12-20 12:09:15 +0200

    Loodud arvude massiiv ja katsetatud testväljastus

---
+++ massiivid.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 20.12.2017
+ * Time: 12:02
+ */
+$arvud = array(1, 5, 2 , 4, 3);
+var_dump($arvud);
+echo '<hr />';
+echo '<pre>';
+print_r($arvud);
+echo '</pre>';
