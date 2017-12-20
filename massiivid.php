0a620e7eb456c312d7d575693a864060d5aa00de
Anna Karutina <anna.karutina@khk.ee>
2017-12-20 12:24:11 +0200

    Massiivitöötluse ülesanded 1. ja 2.

--- massiivid.php
+++ massiivid.php
@@ -33,3 +33,23 @@ echo '<hr />';
 foreach ($arvud as $arv){
     echo '<i>'.$arv.'</i><br />';
 }
+
+/*
+ * 1.
+ * Koosta funktsioon nimega looMassiiv,
+ * mis võtab parameetrina massiivi elementide
+ * arvu ja loob juhuarvudest (täisarvud)
+ * vahemikust 100-999 koosneva massiivi.
+ * Loodud massiiv tuleb tagastada
+ * põhiprogrammile ja kontrollida sisu
+ * testväljastuse abil - näiteks print_r
+ * */
+/*
+ * 2.
+ * Loo funktsioon nimega valjastaMassiiv,
+ * mis võtab parameetrina massiivi ja
+ * väljastab tema elemendid
+ * üheveerulise tabeli kujul
+ * Väljundi loomisel kasutada foreach
+ * tsükkel
+ * */
