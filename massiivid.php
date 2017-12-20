b4cd0bf596617761b8382f4f28abc8935ad51fff
Anna Karutina <anna.karutina@khk.ee>
2017-12-20 12:15:41 +0200

    Loodud massiiv on väljastatud for ja foreach tsükli abil

--- massiivid.php
+++ massiivid.php
@@ -6,8 +6,19 @@
  * Time: 12:02
  */
 $arvud = array(1, 5, 2 , 4, 3);
+
 var_dump($arvud);
 echo '<hr />';
 echo '<pre>';
 print_r($arvud);
 echo '</pre>';
+
+echo '<hr />';
+for($i = 0; $i < count($arvud); $i++){
+    echo '<b>'.$arvud[$i].'</b><br />';
+}
+echo '<hr />';
+foreach ($arvud as $arv){
+    echo '<i>'.$arv.'</i><br />';
+}
+
