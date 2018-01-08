29c1dfd7d472209aae1f6e7751344f8cb532a425
Anna Karutina <anna.karutina@khk.ee>
2018-01-08 08:50:56 +0200

    Defineeritud sidestatud massiiv

---
+++ massiivid2.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 8.01.2018
+ * Time: 8:49
+ */
+$opilane = array(
+    'eesnimi' => 'Mart',
+    'perenimi' => 'Lepp',
+    'vanus' => 16,
+    'klass' => 10
+);
