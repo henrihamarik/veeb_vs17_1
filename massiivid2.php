12f96b9ed86cba031a1bd75933d49b88f65fe90a
Anna Karutina <anna.karutina@khk.ee>
2018-01-08 09:06:34 +0200

    Massiiv on väljastatud foreach tsükliga

--- massiivid2.php
+++ massiivid2.php
@@ -49,3 +49,12 @@ print_r($opilased);
 echo '</pre>';
 
 echo '<hr />';
+
+foreach ($opilased as $opilane){
+    foreach ($opilane as $voti => $vaartus){
+        echo $voti.' - '.$vaartus.'<br />';
+    }
+    echo '---------------'.'<br />';
+}
+
+echo '<hr />';
